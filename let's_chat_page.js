2efa9d886919f941ec0254e057587cc011eb3ef1
VenyaNuthakki <71914805+VenyaNuthakki@users.noreply.github.com>
2021-02-20 09:51:30 +0530

    Add files via upload

--- let's_chat_page.js
+++ let's_chat_page.js
@@ -38,3 +38,26 @@ function logout(){
     localStorage.removeItem("roomnamekey");
     window.location="index.html";
 }
+//Start code
+console.log (firebase_message_id);
+console.log (message_data);
+user_name=message_data["name"];
+message=message_data["msg"];
+like=message_data["like"];
+name_tag="<h3>"+user_name+"<img class='user_tick' src='tick.png'></h3>";
+msg_tag="<h3 class='message_h4'>"+message+"</h3>";
+like_button="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='update_likes(this.id)'>";
+span_tag="<span class='glyphicon glyphicon-thumbs-up'> like:"+like+"</span></button><hr>";
+row=name_tag+msg_tag+like_button+span_tag;
+document.getElementById("output").innerHTML+=row;
+//End code
+} });  }); }
+getData();
+function update_likes(msg_id){
+console.log(msg_id)
+likes=document.getElementById(msg_id).value;
+newlikes=Number(likes)+1;
+firbase.database.ref(room_name).child(msg_id).update({
+      like:newlikes;
+});
+}
